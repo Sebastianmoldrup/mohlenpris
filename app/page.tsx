b2fc6d058e79f97d41cd09b07f1a74b1a72991e5
Sebastianmoldrup <sebastian.moldrup@gmail.com>
2024-02-26 14:35:48 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,184 +1,49 @@
 import Image from 'next/image';
-import allergies from '@/app/data/allergies.json';
 import guests from '@/app/data/guests.json';
 import hosts from '@/app/data/hosts.json';
+import allergies from '@/app/data/allergies.json';
 
 export default function Home() {
-  // // Find the amount of hosts
-  // const hostAmount = hosts.length;
-  // // Find the amount of total guests (individual and group)
-  // const guestAmount =
-  //   Object.values(guests.individuals).length +
-  //   guests.groups.reduce((sum, acc) => sum + acc.group.length, 0);
-  // // Find the base amount of people per host
-  // const baseAmount = Math.floor(guestAmount / hostAmount);
-  // // Find the remainding extra people
-  // const remainder = guestAmount % hostAmount;
-  // // Create an array with the length of the host amount and fill in the base amount
-  // const organizedAmounts = Array(hostAmount).fill(baseAmount);
-  // // Loop through the remainder and add them into the host array so it gets equally distributed
-  // for (let i = 0; i < remainder; i++) {
-  //   organizedAmounts[i]++;
-  // }
+  // console.log(Object.entries(guests));
+  // console.log(Object.entries(hosts));
 
-  // function getGuestsWithAllergy(allergy: string) {
-  //   return guests.individuals.filter((guest) =>
-  //     guest.allergies.includes(allergy)
-  //   );
-  // }
+  const assignGroups = () => {};
 
-  // function getMealWithIngredient(ingredient: string) {
-  //   return hosts.flatMap((host) =>
-  //     Object.entries(host.ingredients).filter(([meal, ingredients]) =>
-  //       Object.values(ingredients).some((ingredientList) =>
-  //         ingredientList.includes(ingredient)
-  //       )
-  //     )
-  //   );
-  // }
-
-  // const allergyMealMap = {};
-
-  // allergies.forEach((allergy) => {
-  //   const guestsWithAllergy = getGuestsWithAllergy(allergy);
-  //   const mealsToAvoid = guestsWithAllergy.flatMap((guest) =>
-  //     guest.allergies.flatMap((allergy) => getMealWithIngredient(allergy))
-  //   );
-  //   const allergyMealMap: { [key: string]: any } = mealsToAvoid;
-  // });
-
-  // console.log(allergyMealMap);
-
-  // const guestsWithAllergy = [];
-  // const guestsWithoutAllergy = [];
-  // function getGuestsWithAllergy(allergy: string) {
-  //   guests.individuals.map((individual) => {
-  //     if (individual.allergies.includes(allergy))
-  //       guestsWithAllergy.push(individual);
-  //     else if (individual.allergies.length === 0)
-  //       guestsWithoutAllergy.push(individual);
-  //   });
-  // }
-
-  // const mealsWithIngredient = [];
-  // function getMealWithIngredient(ingredient: string) {
-  //   hosts.map((host) => {
-  //     Object.entries(host.ingredients).map(([meal, ingredients]) => {
-  //       Object.values(ingredients).map((ingredientList) => {
-  //         if (ingredientList.includes(ingredient))
-  //           mealsWithIngredient.push(meal);
-  //       });
-  //     });
-  //   });
-  // }
-
-  // guests.groups.map((group) => {
-  //   console.log(group);
-  // });
-
-  function getGuestsWithAllergy() {
-    guests.individuals.map((individual) => {
-      allergies.map((allergy) => {
-        if (individual.allergies.includes(allergy)) {
-          console.log(individual, allergy);
-        }
-      });
-    });
-
-    guests.groups.map((group) => {
-      allergies.map((allergy) => {
-        if (group.allergies.includes(allergy)) {
-          console.log(group, allergy);
-        }
+  function assignAppetizer() {
+    Object.values(hosts).map((host) => {
+      // console.log(host);
+      // Check all the ingredients
+      host.ingredients.map((ingredient) => {
+        // console.log(ingredient);
+        // Check all the guest groups allergies
+        guests.groups.map((group) => {
+          // console.log(Object.entries(group.allergies));
+          // Check if the ingredient is in the group allergies
+          Object.entries(group.allergies).map(([key, allergy], index) => {
+            // console.log(allergy);
+            // console.log(ingredient.appetizer);
+            ingredient.appetizer.includes(allergy) ? console.log('yes') : null;
+          });
+        });
       });
     });
   }
-
-  getGuestsWithAllergy();
+  assignAppetizer();
 
   return (
     <main className='flex flex-col min-h-screen items-start justify-center mt-10 gap-x-10'>
-      <div className='w-full h-[500px]'></div>
-
-      <div className='flex items-start justify-center gap-x-10 font-sm w-full'>
-        <div className='space-y-4'>
-          <h2 data-name='md-title' className=''>
-            Allergener:
-          </h2>
-          <ul className='space-y-2'>
-            {allergies.map((allergy, index) => (
-              <li key={index}>{allergy}</li>
-            ))}
-          </ul>
-        </div>
-
-        <div className='space-y-4'>
-          <h2 data-name='md-title' className=''>
-            Vertskap:
-          </h2>
-          <ul className='space-y-6'>
-            {hosts.map((host, index) => (
-              <li key={index}>
-                <h3 data-name='sm-title' className=''>
-                  {host.address}
-                </h3>
-                <ul className='flex gap-x-4'>
-                  {host.names.map((name, index) => (
-                    <li key={index}>{name}</li>
-                  ))}
-                </ul>
-                <div className='flex gap-x-4'>
-                  {host.ingredients.map((ingredient, index) => (
-                    <div key={index}>
-                      <div>{ingredient.appetizer}</div>
-                      <div>{ingredient.main}</div>
-                      <div>{ingredient.dessert}</div>
-                    </div>
-                  ))}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className='space-y-4'>
-          <h2 data-name='md-title' className=''>
-            Gjester:
-          </h2>
-          <h3>Singel:</h3>
-          <ul className='space-y-4'>
-            {guests.individuals.map((person, index) => (
-              <li key={index}>
-                <h3 data-name='sm-title' className=''>
-                  {person.name}
-                </h3>
-                <div className=''>
-                  {person.allergies.map((allergy, index) => (
-                    <div key={index}>{allergy}</div>
-                  ))}
-                </div>
-              </li>
-            ))}
-          </ul>
-          <h3>Grupper:</h3>
-          {guests.groups.map((group, index) => (
-            <div key={index}>
-              <ul className='flex gap-x-2'>
-                {group.names.map((name, index) => (
-                  <li data-name='sm-title' key={index}>
-                    {name}
-                  </li>
-                ))}
-              </ul>
-              <ul className='flex gap-x-2'>
-                {group.allergies.map((allergy, index) => (
-                  <li key={index}>{allergy}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      </div>
+      <ul className='flex space-x-4'>
+        {allergies.map((allergy, index) => {
+          return (
+            <li
+              className='px-4 py-2 bg-white text-black rounded-md hover:text-white hover:bg-black hover:cursor-pointer'
+              key={index}
+            >
+              {allergy}
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
