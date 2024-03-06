0e16482487f4bf65ffe8c93a07f8ce3af00cfd62
Sebastian Møldrup <sebastianmoldrup@Sebastians-Air.home>
2024-03-06 09:00:46 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -2,77 +2,110 @@
 import Image from 'next/image';
 import guests from '@/app/data/guests.json';
 import hosts from '@/app/data/hosts.json';
-import allergies from '@/app/data/allergies.json';
 import List from '@/components/list';
-// import { SignUpGuest } from '@/components/signUpGuest';
 import { useState, useEffect } from 'react';
 
 export default function Home() {
-  // State to store the access token
-  const [accessToken, setAccessToken] = useState(null);
+  // // Access token state
+  // const [accessToken, setAccessToken] = useState(null);
 
-  // State to store organizers, events, or any other data you retrieve
-  const [organizers, setOrganizers] = useState([]);
+  // // Organizers state
+  // const [organizers, setOrganizers] = useState([]);
 
-  // Effect to obtain the access token on component mount
-  useEffect(() => {
-    const fetchAccessToken = async () => {
-      try {
-        const response = await fetch('https://legacyapi.deltager.no/token', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: 'grant_type=password&username={username}&password={password}&affid=1',
-        });
+  // // Obtain the access token on component mount
+  // useEffect(() => {
+  //   const fetchAccessToken = async () => {
+  //     try {
+  //       const response = await fetch('https://legacyapi.deltager.no/token', {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //         },
+  //         body: 'grant_type=password&username={tourdemohlenpris@gmail.com}&password={Skjalg1973}&affid=1',
+  //       });
 
-        if (!response.ok) {
-          throw new Error('Failed to obtain access token');
-        }
+  //       if (!response.ok) {
+  //         throw new Error('Failed to obtain access token');
+  //       }
 
-        const data = await response.json();
-        setAccessToken(data.access_token);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  //       const data = await response.json();
+  //       setAccessToken(data.access_token);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
 
-    fetchAccessToken();
-  }, []); // Empty dependency array ensures this effect runs once on component mount
+  //   fetchAccessToken();
+  // }, []); 
 
-  // Effect to fetch organizers when access token is obtained
-  useEffect(() => {
-    const fetchOrganizers = async () => {
-      try {
-        const response = await fetch(
-          'https://legacyapi.deltager.no/organizers',
-          {
-            headers: {
-              Authorization: `bearer ${accessToken}`,
-            },
-          }
-        );
+  // // Fetch organizers when access token is obtained
+  // useEffect(() => {
+  //   const fetchOrganizers = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         'https://legacyapi.deltager.no/organizers',
+  //         {
+  //           headers: {
+  //             Authorization: `bearer ${accessToken}`,
+  //           },
+  //         }
+  //       );
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch organizers');
-        }
+  //       if (!response.ok) {
+  //         throw new Error('Failed to fetch organizers');
+  //       }
 
-        const data = await response.json();
-        setOrganizers(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  //       const data = await response.json();
+  //       setOrganizers(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
 
-    // Fetch organizers only if the access token is available
-    if (accessToken) {
-      fetchOrganizers();
-    }
-  }, [accessToken]); // Run this effect whenever access token changes
+  //   // Fetch organizers only if the access token is available
+  //   if (accessToken) {
+  //     fetchOrganizers();
+  //   }
+  // }, [accessToken]); 
+
+    const [courses, setCourses] = useState([
+      { name: 'appetizer', data: [] },
+      { name: 'main', data: [] },
+      { name: 'dessert', data: [] },
+    ]);
+  
+  function sortAppetizer() {
+    return hosts.map((host) => {
+      guests.map((guest) => {
+        guest.allergies.map((allergy) => {
+          if (host.ingredients.appetizer.includes(allergy)) {
+            return courses;
+          } else {
+            setCourses(courses.map((course) => {
+              course.name === 'appetizer' 
+              ? { ...course, data: [...course.data, host] }
+              : course
+            }))
+            }
+        })
+      });
+    })
+  }
+
+  sortAppetizer();
+
+  function sortMainCourse() {
+    return hosts
+    .filter((host) => console.log(host.ingredients.main))
+  }
+
+  function sortDessert() {
+    return hosts
+    .filter((host) => console.log(host.ingredients.dessert))
+  }
 
   return (
     <main className='flex flex-col min-h-screen items-center justify-center mt-10 gap-x-10'>
-      {/* <SignUpGuest /> */}
       <List />
     </main>
   );
